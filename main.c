e777e994aa7bf90cfe65e00e253775c7ecee654a
David Kennedy <kennedyd@udel.edu>
2025-03-20 23:40:57 -0400

    Resolved errors

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 8080
 
 int init_socket(void);
 
 int main(void){
-
+	init_socket();
 }
 
 int init_socket(void){
@@ -15,8 +16,8 @@ int init_socket(void){
 	struct sockaddr_in server_addr;	// socket config
 
 	// create server socket
-	server_fd = socket(AF_INET, SOCK_STREAM, 0); // Initialize the socket
-	if(server_fd < 0){	// Check if initialization failed
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0); // Initialize the socket
+	if(socket_fd < 0){	// Check if initialization failed
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
@@ -27,14 +28,14 @@ int init_socket(void){
 	server_addr.sin_port = htons(PORT);			// Set the port to bind the socket too
 
 	// bind socket to port
-	if(bind(socket_fd, &server_addr, size_of(server_addr)) < 0){ /* Attempt to bind socket 
+	if(bind(socket_fd, &server_addr, sizeof(server_addr)) < 0){ /* Attempt to bind socket 
 																	and check for failure */
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Listen for connections
-	if(listen(server_fd, 10) < 0){ /* Listen for traffic
+	if(listen(socket_fd, 10) < 0){ /* Listen for traffic
 									  and check for failure. */
 		perror("listen failed");
 		exit(EXIT_FAILURE);
